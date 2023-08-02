1eb31d1fe3b06340c833ea9df5d13b46cbd5c561
AlexKarmHon <89stigmat89@gmail.com>
2023-08-02 21:46:41 +0300

    mobile menu done

--- js/headerheight.js
+++ js/headerheight.js
@@ -1,4 +1,7 @@
 const { height: pageHeaderHeight } = document
       .querySelector(".header")
       .getBoundingClientRect();
-document.body.style.paddingTop = `${pageHeaderHeight}px`
+document.body.style.paddingTop = `${pageHeaderHeight}px`;
+
+const menu = document.getElementById("mbm");
+menu.style.top = `${pageHeaderHeight}px`;
